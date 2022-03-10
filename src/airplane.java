e56b229ce5e5a9288f40202b103c87971399872f
Boris Aleksandrov <aleksandrov.bg78@gmail.com>
2022-03-10 15:46:44 +0900

    Создал класс airplane, добавил методы для полета и отображения местоположения

--- src/airplane.java
+++ src/airplane.java
@@ -1,3 +1,62 @@
+import java.util.Random;
+
 public class airplane {
+
+    Random rnd = new Random();
+
+    private int x,y,z;
+    private static int fotonum;
+    private int fuel;
+    private int dir = 0;
+    private int r = rnd.nextInt();
+
+
+    public airplane(int x, int y, int fuel) {
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
+    }
+
+    public void flying(int distance) {
+
+        if (distance > fuel) {
+            distance = fuel;
+        }
+        fuel -= distance;
+
+        if (dir == 0) {
+            x += distance;
+            y += 0;
+        } else if (dir == 1) {
+            x += 0;
+            y += distance;
+        } else if (dir == 2) {
+            x -= distance;
+            y += 0;
+        } else if (dir == 3) {
+            x += 0;
+            y -= distance;
+        }
+
+
+    }
+
+    public void turnRigt (){
+        dir++;
+        if (dir == 4) {
+            dir = 0;
+        }
+    }
+
+    void turnLeft () {
+        dir--;
+        if (dir == -1) {
+            dir = 3;
+        }
+    }
+
+    public void showPosition () {
+        System.out.println("Your position x= " + x + ", y= " + y );
+    }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,13 @@
 public class main {
+
+    public static void main(String[] args) {
+        airplane planer = new airplane(8,9,20);
+
+        planer.flying(6);
+        planer.showPosition();
+
+    }
+
+
+
 }
